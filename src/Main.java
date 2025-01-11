3ffea6a9ea1eacc7a1d9498a30541623e9c9139f
Alex-Java-coder <alexbochkarev321@gmail.com>
2025-01-11 12:29:15 +0300

    Firs2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         SubClass obj = new SubClass();
         obj.myMethod();
-
+        System.out.println("djfhksjd");
     }
 }
