ccee029508cef667143170af2627846f978db6ae
Alex-Java-coder <alexbochkarev321@gmail.com>
2025-01-11 11:09:21 +0300

    first

---
+++ src/SuperClass.java
@@ -0,0 +1,8 @@
+public class SuperClass {
+    int num = 88;
+
+    // Метод display() суперкласса
+    public void display() {
+        System.out.println("Это метод display() суперкласса");
+    }
+}
