5684fed2a75d200d92d9c7583f5cc01be9c4f3d4
Alex-Java-coder <alexbochkarev321@gmail.com>
2025-01-14 00:07:43 +0300

    Nedely8 s dobavkoi 3

--- src/Main.java
+++ src/Main.java
@@ -132,7 +132,23 @@ public class Main {
 
         System.out.println(" ");
 
+        //вот это битва
         Fight fight = new Fight();
         fight.main(args);
+
+        System.out.println("\n");
+
+        Employee employee = new Employee();
+        Programmer programmer = new Programmer();
+        Designer designer = new Designer();
+        Employee empProg = new Programmer("Meow Moore", "Программист", 30, 5,
+                100_000, "Java");
+        Employee empDes = new Designer("Super Mors", "Дизайнер", 25, 3,
+                70_000, "Adobe Illustrator");
+        empProg.getTextInfo();
+        System.out.println();
+        empDes.getTextInfo();
+        employee.getCountOfEmp();
+
     }
 }
